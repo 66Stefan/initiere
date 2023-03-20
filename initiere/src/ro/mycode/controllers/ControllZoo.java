e32c205c2e260707ae026806cb0abc7a3f09a9da
Stefan Partenie <stefanpartenie@gmail.com>
2023-03-20 21:47:49 +0200

    first day - revolve IntelliJ problems

--- initiere/src/ro/mycode/controllers/ControllZoo.java
+++ initiere/src/ro/mycode/controllers/ControllZoo.java
@@ -4,93 +4,35 @@ import ro.mycode.models.Zoo;
 
 import java.util.ArrayList;
 public class ControllZoo {
+    private ArrayList<Zoo> animal;
 
-    public static ArrayList<Zoo> animal = new ArrayList<>();
+    public  ControllZoo(){
+        this.animal = new ArrayList<>();
+        this.load();
+    }
 
-//    public static void load(){
-//
-//
-//
-//        Zoo animal1 = new Zoo();
-//        Zoo animal2 = new Zoo();
-//        Zoo animal3 =new Zoo();
-//        Zoo animal4= new Zoo();
-//
-//        animal1.numeAnimal="Tigru";
-//        animal1.ceMananca="Carne";
-//        animal1.stareaDeSanatate = "Sanatos";
-//        animal1.nrCusca=12;
-//        animal1.varsta =12;
-//
-//
-//        animal2.numeAnimal="maimuta";
-//        animal2.ceMananca="Banane";
-//        animal2.stareaDeSanatate = "Conjunctivita";
-//        animal2.nrCusca=1;
-//        animal2.varsta =3;
-//
-//        animal3.numeAnimal="Urs";
-//        animal3.ceMananca="Carne si Legume";
-//        animal3.stareaDeSanatate = "Sanatos";
-//        animal3.nrCusca=3;
-//        animal3.varsta =15;
-//
-//        animal4.numeAnimal="Iepure";
-//        animal4.ceMananca="Legume";
-//        animal4.stareaDeSanatate = "Piceor Rupt";
-//        animal4.nrCusca=33;
-//        animal4.varsta =22;
-//
-//        animal.add(animal1);
-//        animal.add(animal2);
-//        animal.add(animal3);
-//        animal.add(animal4);
-//    }
-//
-//    //todo: afisaza un anume animal daca exista
-//
-//    public Zoo couaterAnimalDupaNume(String numeAnimal){
-//
-//        for(int i=0; i<animal.size();i++){
-//            if(animal.get(i).numeAnimal.equals(numeAnimal)){
-//                return animal.get(i);
-//            }
-//        }
-//        return null;
-//    }
-//
-//    //todo: afisaza animalele din cusca mai mare de 5
-//
-//    public void animaleDinCuscaMaiMareDe5(){
-//
-//        for(int i=0; i<animal.size();i++){
-//            if(animal.get(i).nrCusca>5){
-//                System.out.print(animal.get(i).afisareZoo());
-//            }
-//        }
-//    }
-//
-//
-////todo:afisaza cel mai batran animal
-//    public Zoo celMaiBatranAnimal(){
-//        Zoo animalBatran=this.animal.get(0);
-//        for(int i=0;i<animal.size();i++){
-//
-//            if(animalBatran.varsta<animal.get(i).varsta){
-//                animalBatran=animal.get(i);
-//            }
-//        }
-//        return animalBatran;
-//    }
-//
-//
-//
-//
-//
+    public void  load(){
+        Zoo animal1 = new Zoo("Leu", "Carne", "Sanatos", 12, 8);
+        Zoo animal2 = new Zoo("Zebra", "Vegetarian", "Sanatos", 3, 5);
+        Zoo animal3 = new Zoo("Urs", "Omnivor", "Durere masea", 1, 7);
+        Zoo animal4 = new Zoo("Maimuta", "Banane", "Sanatos", 17, 11);
+        Zoo animal5 = new Zoo("Papagal", "Seminte", "Nu doarme", 22, 6);
 
+        this.animal.add(animal1);
+        this.animal.add(animal2);
+        this.animal.add(animal3);
+        this.animal.add(animal4);
+        this.animal.add(animal5);
+        }
 
+    //todo afisare
 
+    public void afisareZoo() {
+        for (int i = 0; i < animal.size();i++){
+            System.out.println(animal.get(i).afisareZoo());
+        }
 
+    }
 
 
 
